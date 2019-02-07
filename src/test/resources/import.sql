5cb35237a90d95f44ca73a6a47e065ab369283a2
yanbigot <yanbigot@gmail.com>
2019-02-07 23:40:25 +0100

    test embedded postgres

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,59 +1,59 @@
-CREATE SCHEMA IF NOT EXISTS BDD_SCHEMA;
-
-DROP TABLE IF EXISTS BDD_SCHEMA.MANAGER_COLLABORATOR;
-DROP TABLE IF EXISTS BDD_SCHEMA.EMPLOYEES;
-DROP TABLE IF EXISTS BDD_SCHEMA.FOLLOWUPS;
-DROP TABLE IF EXISTS BDD_SCHEMA.TRAININGS;
-DROP TABLE IF EXISTS BDD_SCHEMA.CURRICULUMS;
-
-CREATE TABLE IF NOT EXISTS BDD_SCHEMA.MANAGER_COLLABORATOR(
-  ggiManager varchar(20) NOT NULL,
-  ggiCollaborator varchar(20) NOT NULL
- );
-CREATE TABLE IF NOT EXISTS BDD_SCHEMA.EMPLOYEES(
-merge_key varchar(20) NOT NULL,
-data_map json NOT NULL
-);
-CREATE TABLE IF NOT EXISTS BDD_SCHEMA.FOLLOWUPS(
-  merge_key varchar(20) NOT NULL,
-  data_map json NOT NULL
- );
-CREATE TABLE IF NOT EXISTS BDD_SCHEMA.TRAININGS(
-  merge_key varchar(20) NOT NULL,
-  data_map json NOT NULL
- );
- CREATE TABLE IF NOT EXISTS BDD_SCHEMA.CURRICULUMS(
-  merge_key varchar(20) NOT NULL,
-  data_map json NOT NULL
- );
-
-
-INSERT INTO BDD_SCHEMA.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '10');
-INSERT INTO BDD_SCHEMA.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '11');
-INSERT INTO BDD_SCHEMA.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '12');
-INSERT INTO BDD_SCHEMA.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '13');
-INSERT INTO BDD_SCHEMA.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('2', '20');
-
-INSERT INTO BDD_SCHEMA.EMPLOYEES(merge_key, data_map) VALUES('10', '{ "ggi": "10", "name": "name"}');
-INSERT INTO BDD_SCHEMA.EMPLOYEES(merge_key, data_map) VALUES('11', '{ "ggi": "11", "name": "name"}');
-INSERT INTO BDD_SCHEMA.EMPLOYEES(merge_key, data_map) VALUES('12', '{ "ggi": "12", "name": "name"}');
-INSERT INTO BDD_SCHEMA.EMPLOYEES(merge_key, data_map) VALUES('13', '{ "ggi": "13", "name": "name"}');
-INSERT INTO BDD_SCHEMA.EMPLOYEES(merge_key, data_map) VALUES('20', '{ "ggi": "20", "name": "name"}');
-
-INSERT INTO BDD_SCHEMA.FOLLOWUPS(merge_key, data_map) VALUES('10', '{"ggi": "10", "trainingId": "t_10", "version": "1"}');
-INSERT INTO BDD_SCHEMA.FOLLOWUPS(merge_key, data_map) VALUES('11', '{"ggi": "11", "trainingId": "t_11", "version": "1"}');
-INSERT INTO BDD_SCHEMA.FOLLOWUPS(merge_key, data_map) VALUES('12', '{"ggi": "12", "trainingId": "t_12", "version": "1"}');
-INSERT INTO BDD_SCHEMA.FOLLOWUPS(merge_key, data_map) VALUES('13', '{"ggi": "13", "trainingId": "t_13", "version": "1"}');
-INSERT INTO BDD_SCHEMA.FOLLOWUPS(merge_key, data_map) VALUES('20', '{"ggi": "20", "trainingId": "t_20", "version": "1"}');
-
-INSERT INTO BDD_SCHEMA.TRAININGS(merge_key, data_map) VALUES('t_10', '{ "trainingId": "t_10", "version": "1", "typeCode": "PRGM"}');
-INSERT INTO BDD_SCHEMA.TRAININGS(merge_key, data_map) VALUES('t_11', '{ "trainingId": "t_11", "version": "1", "typeCode": "CRSE"}');
-INSERT INTO BDD_SCHEMA.TRAININGS(merge_key, data_map) VALUES('t_12', '{ "trainingId": "t_12", "version": "1", "typeCode": "CRSE"}');
-INSERT INTO BDD_SCHEMA.TRAININGS(merge_key, data_map) VALUES('t_13', '{ "trainingId": "t_13", "version": "1", "typeCode": "CRSE"}');
-INSERT INTO BDD_SCHEMA.TRAININGS(merge_key, data_map) VALUES('t_20', '{ "trainingId": "t_20", "version": "1", "typeCode": "CRSE"}');
-
---INSERT INTBDD_SCHEMA.O curriculums(merge_key, data_map) VALUES('t_10', '{ "trainingId": "t_10", "curriculumId": "t_10"}}');
-INSERT INTO BDD_SCHEMA.CURRICULUMS(merge_key, data_map) VALUES('t_11', '{ "trainingId": "t_11", "curriculumId": "t_10"}');
-INSERT INTO BDD_SCHEMA.CURRICULUMS(merge_key, data_map) VALUES('t_12', '{ "trainingId": "t_12", "curriculumId": "t_10"}');
-INSERT INTO BDD_SCHEMA.CURRICULUMS(merge_key, data_map) VALUES('t_13', '{ "trainingId": "t_13", "curriculumId": "t_10"}');
-INSERT INTO BDD_SCHEMA.CURRICULUMS(merge_key, data_map) VALUES('t_20', '{ "trainingId": "t_20", "curriculumId": "t_10"}');
+c.createStatement().execute("CREATE IF NOT EXISTS SCHEMA bdd_schema;
+c.createStatement().execute("
+c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.MANAGER_COLLABORATOR;
+c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.EMPLOYEES;
+c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.FOLLOWUPS;
+c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.TRAININGS;
+c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.CURRICULUMS;
+c.createStatement().execute("
+c.createStatement().execute("CREATE TABLE IF NOT EXISTS bdd_schema.MANAGER_COLLABORATOR(
+c.createStatement().execute("  ggiManager varchar(20) NOT NULL,
+c.createStatement().execute("  ggiCollaborator varchar(20) NOT NULL
+c.createStatement().execute(" );
+c.createStatement().execute("CREATE TABLE IF NOT EXISTS bdd_schema.EMPLOYEES(
+c.createStatement().execute("merge_key varchar(20) NOT NULL,
+c.createStatement().execute("data_map json NOT NULL
+c.createStatement().execute(");
+c.createStatement().execute("CREATE TABLE IF NOT EXISTS bdd_schema.FOLLOWUPS(
+c.createStatement().execute("  merge_key varchar(20) NOT NULL,
+c.createStatement().execute("  data_map json NOT NULL
+c.createStatement().execute(" );
+c.createStatement().execute("CREATE TABLE IF NOT EXISTS bdd_schema.TRAININGS(
+c.createStatement().execute("  merge_key varchar(20) NOT NULL,
+c.createStatement().execute("  data_map json NOT NULL
+c.createStatement().execute(" );
+c.createStatement().execute(" CREATE TABLE IF NOT EXISTS bdd_schema.CURRICULUMS(
+c.createStatement().execute("  merge_key varchar(20) NOT NULL,
+c.createStatement().execute("  data_map json NOT NULL
+c.createStatement().execute(" );
+c.createStatement().execute("
+c.createStatement().execute("
+c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '10');
+c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '11');
+c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '12');
+c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '13');
+c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('2', '20');
+c.createStatement().execute("
+c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('10', '{ "ggi": "10", "name": "name"}');
+c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('11', '{ "ggi": "11", "name": "name"}');
+c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('12', '{ "ggi": "12", "name": "name"}');
+c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('13', '{ "ggi": "13", "name": "name"}');
+c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('20', '{ "ggi": "20", "name": "name"}');
+c.createStatement().execute("
+c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('10', '{"ggi": "10", "trainingId": "t_10", "version": "1"}');
+c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('11', '{"ggi": "11", "trainingId": "t_11", "version": "1"}');
+c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('12', '{"ggi": "12", "trainingId": "t_12", "version": "1"}');
+c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('13', '{"ggi": "13", "trainingId": "t_13", "version": "1"}');
+c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('20', '{"ggi": "20", "trainingId": "t_20", "version": "1"}');
+c.createStatement().execute("
+c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_10', '{ "trainingId": "t_10", "version": "1", "typeCode": "PRGM"}');
+c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_11', '{ "trainingId": "t_11", "version": "1", "typeCode": "CRSE"}');
+c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_12', '{ "trainingId": "t_12", "version": "1", "typeCode": "CRSE"}');
+c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_13', '{ "trainingId": "t_13", "version": "1", "typeCode": "CRSE"}');
+c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_20', '{ "trainingId": "t_20", "version": "1", "typeCode": "CRSE"}');
+c.createStatement().execute("
+c.createStatement().execute("--INSERT INTbdd_schema.O curriculums(merge_key, data_map) VALUES('t_10', '{ "trainingId": "t_10", "curriculumId": "t_10"}}');
+c.createStatement().execute("INSERT INTO bdd_schema.CURRICULUMS(merge_key, data_map) VALUES('t_11', '{ "trainingId": "t_11", "curriculumId": "t_10"}');
+c.createStatement().execute("INSERT INTO bdd_schema.CURRICULUMS(merge_key, data_map) VALUES('t_12', '{ "trainingId": "t_12", "curriculumId": "t_10"}');
+c.createStatement().execute("INSERT INTO bdd_schema.CURRICULUMS(merge_key, data_map) VALUES('t_13', '{ "trainingId": "t_13", "curriculumId": "t_10"}');
+c.createStatement().execute("INSERT INTO bdd_schema.CURRICULUMS(merge_key, data_map) VALUES('t_20', '{ "trainingId": "t_20", "curriculumId": "t_10"}');
--- src/test/scala/database/PgTest.java
+++ src/test/scala/database/PgTest.java
@@ -32,9 +32,44 @@ public class PgTest {
         return DriverManager.getConnection(url);
     }
 
-    public static void load(){
-        postgres.getProcess().get().importFromFile(new File("import.sql"));
-
+    public static void load(Connection c) throws SQLException {
+//        postgres.getProcess().get().importFromFile(new File("import.sql"));
+        c.createStatement().execute("CREATE  SCHEMA bdd_schema;");
+        c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.MANAGER_COLLABORATOR;");
+        c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.EMPLOYEES;");
+        c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.FOLLOWUPS;");
+        c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.TRAININGS;");
+        c.createStatement().execute("DROP TABLE IF EXISTS bdd_schema.CURRICULUMS;");
+        c.createStatement().execute("CREATE TABLE bdd_schema.MANAGER_COLLABORATOR(ggiManager varchar(20) NOT NULL,ggiCollaborator varchar(20) NOT NULL);");
+        c.createStatement().execute("CREATE TABLE bdd_schema.EMPLOYEES(merge_key varchar(20) NOT NULL,data_map json NOT NULL);");
+        c.createStatement().execute("CREATE TABLE bdd_schema.FOLLOWUPS(merge_key varchar(20) NOT NULL,data_map json NOT NULL);");
+        c.createStatement().execute("CREATE TABLE bdd_schema.TRAININGS(merge_key varchar(20) NOT NULL,data_map json NOT NULL);");
+        c.createStatement().execute(" CREATE TABLE bdd_schema.CURRICULUMS(merge_key varchar(20) NOT NULL,data_map json NOT NULL);");
+        c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '10');");
+        c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '11');");
+        c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '12');");
+        c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('1', '13');");
+        c.createStatement().execute("INSERT INTO bdd_schema.MANAGER_COLLABORATOR(ggiManager, ggiCollaborator) VALUES('2', '20');");
+        c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('10', '{ \"ggi\": \"10\", \"name\": \"name\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('11', '{ \"ggi\": \"11\", \"name\": \"name\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('12', '{ \"ggi\": \"12\", \"name\": \"name\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('13', '{ \"ggi\": \"13\", \"name\": \"name\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.EMPLOYEES(merge_key, data_map) VALUES('20', '{ \"ggi\": \"20\", \"name\": \"name\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('10', '{\"ggi\": \"10\", \"trainingId\": \"t_10\", \"version\": \"1\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('11', '{ \"ggi\": \"11\", \"trainingId\": \"t_11\", \"version\": \"1\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('12', '{\"ggi\": \"12\", \"trainingId\": \"t_12\", \"version\": \"1\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('13', '{\"ggi\": \"13\", \"trainingId\": \"t_13\", \"version\": \"1\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.FOLLOWUPS(merge_key, data_map) VALUES('20', '{\"ggi\": \"20\", \"trainingId\": \"t_20\", \"version\": \"1\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_10', '{ \"trainingId\": \"t_10\", \"version\": \"1\", \"typeCode\": \"PRGM\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_11', '{ \"trainingId\": \"t_11\", \"version\": \"1\", \"typeCode\": \"CRSE\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_12', '{ \"trainingId\": \"t_12\", \"version\": \"1\", \"typeCode\": \"CRSE\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_13', '{ \"trainingId\": \"t_13\", \"version\": \"1\", \"typeCode\": \"CRSE\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.TRAININGS(merge_key, data_map) VALUES('t_20', '{ \"trainingId\": \"t_20\", \"version\": \"1\", \"typeCode\": \"CRSE\"}');");
+        c.createStatement().execute("--INSERT INTbdd_schema.O curriculums(merge_key, data_map) VALUES('t_10', '{ \"trainingId\": \"t_10\", \"curriculumId\": \"t_10\"}}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.CURRICULUMS(merge_key, data_map) VALUES('t_11', '{ \"trainingId\": \"t_11\", \"curriculumId\": \"t_10\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.CURRICULUMS(merge_key, data_map) VALUES('t_12', '{ \"trainingId\": \"t_12\", \"curriculumId\": \"t_10\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.CURRICULUMS(merge_key, data_map) VALUES('t_13', '{ \"trainingId\": \"t_13\", \"curriculumId\": \"t_10\"}');");
+        c.createStatement().execute("INSERT INTO bdd_schema.CURRICULUMS(merge_key, data_map) VALUES('t_20', '{ \"trainingId\": \"t_20\", \"curriculumId\": \"t_10\"}');");
     }
 
     public static String queryFromFile(String path) throws IOException {
@@ -56,19 +91,27 @@ public class PgTest {
         String url = startInstance();
         try {
             final Connection conn = getConnection(url);
-            load();
+            conn.setAutoCommit(true);
+            load(conn);
+
+//            final Statement createSchema = conn.createStatement();
+//            createSchema.execute("CREATE SCHEMA BDD_SCHEMA;" );
+
             final Statement crtlStatement = conn.createStatement();
-            crtlStatement.execute("select schema_name\n" +
-                    "from information_schema.schemata");
+            crtlStatement.execute("select schema_name from information_schema.schemata");
             crtlStatement.getResultSet();
             while(crtlStatement.getResultSet().next()){
                 System.out.println("[" + crtlStatement.getResultSet().getString(1));
             }
+
             final Statement statement = conn.createStatement();
-            String query = queryFromFile("D:\\SCALA_WORKSPACE\\finatra-postgres\\src\\test\\resources\\a.sql");
+            String query = queryFromFile("D:\\SCALA_WORKSPACE\\finatra-postgres\\src\\test\\resources\\query.sql");
             statement.execute(query);
             while(statement.getResultSet().next()){
-                System.out.println("[" + statement.getResultSet() + "]");
+                System.out.println("[" + statement.getResultSet().getString(0) + "]");
+                System.out.println("[" + statement.getResultSet().getString(1) + "]");
+                System.out.println("[" + statement.getResultSet().getString(2) + "]");
+                System.out.println("[" + statement.getResultSet().getString(3) + "]");
             }
             conn.close();
         } catch (SQLException e) {
